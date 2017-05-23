8e737696634f99e9640256369b160a74aa52b52c
Dave Strus <dave@getfretless.com>
2017-05-23 15:54:31 -0400

    Add Header component and style

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>ThingList</h1>
+      <h2>So Many Things</h2>
+    </header>
+  )
+}
+
+export default Header
